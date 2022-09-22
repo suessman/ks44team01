85666e76d27981ca50e884c141a7a0dddddfe71c
suessman <suessman11@gmail.com>
2022-09-22 10:04:09 +0900

    first commit

---
+++ ksmartteam01/src/main/java/anypet/ksmartteam01/controller/MainController.java
@@ -0,0 +1,17 @@
+package anypet.ksmartteam01.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class MainController {
+	@GetMapping("/")
+	public String main(Model model) {
+		return "main";
+	}
+}
+
+
+
+
