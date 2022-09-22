05a93203fd2a8fc7a41567258db29bb5167db191
heeomheeom <hyoun_7@naver.com>
2022-09-22 17:36:32 +0900

    [ks44team01] 숙소 - 숙소목록 author: 박주현 file: accommodationList.html detail: 숙소목록_진행중

--- ksmartteam01/src/main/java/anypet/ksmartteam01/admin/controller/AccommodationListController.java
+++ ksmartteam01/src/main/java/anypet/ksmartteam01/admin/controller/AccommodationListController.java
@@ -1,5 +1,21 @@
 package anypet.ksmartteam01.admin.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/accommodation")
 public class AccommodationListController {
+
+	/**
+	 * 숙소 목록
+	 */	
+	@GetMapping("/accommodationList")
+	public String getAccommodationList(Model model) {
+		return "/user/accommodation/accommodationList";
+	}
 
 }
