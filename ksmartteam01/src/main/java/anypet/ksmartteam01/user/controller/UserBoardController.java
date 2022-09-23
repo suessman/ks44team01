dfb94e6b016582b660cf35f504e0906d6a82a841
SimJeong <sjm4324@gmail.com>
2022-09-23 17:24:11 +0900

    main & boardList

--- ksmartteam01/src/main/java/anypet/ksmartteam01/user/controller/UserBoardController.java
+++ ksmartteam01/src/main/java/anypet/ksmartteam01/user/controller/UserBoardController.java
@@ -1,5 +1,15 @@
 package anypet.ksmartteam01.user.controller;
 
-public class UserBoardController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/board")
+public class UserBoardController {
+	
+	@GetMapping("/boardList")
+	public String boardList() {
+		return "/user/board/boardList";
+	}
 }
