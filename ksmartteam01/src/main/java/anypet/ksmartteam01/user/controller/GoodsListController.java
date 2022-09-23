4ef4a3cbd895b978ed9a6b6e2e952ef11179bc12
StationOfLifeProject <StationOfLifeProject@gmail.com>
2022-09-23 17:26:58 +0900

    상품목록, 장바구니 진행중

--- ksmartteam01/src/main/java/anypet/ksmartteam01/user/controller/GoodsListController.java
+++ ksmartteam01/src/main/java/anypet/ksmartteam01/user/controller/GoodsListController.java
@@ -1,5 +1,49 @@
 package anypet.ksmartteam01.user.controller;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/goods")
 public class GoodsListController {
 
+	@GetMapping("/goodsInsert")
+	public String goodsInsert() {
+	
+		return "/user/goods/goodsInsert";
+	}
+	@GetMapping("/goodsList")
+	public String goodList() {
+		
+		return "/user/goods/goodsList";
+	}
+	@GetMapping("/goodsListUser")
+	public String goodsList() {
+		
+		return  "/user/goods/goodsListUser";
+	}
+	@GetMapping("/goodsModify")
+	public String goodsModify() {
+	
+		return "/user/goods/goodsModify";
+	}
+	@GetMapping("/goodsShippingInfoInsert")
+	public String goodsShippingInfoInsert() {
+		
+		return "/user/goods/goodsShippingInfoInsert";
+	}
+	@GetMapping("/goodsShippingInfoModify")
+	public String goodsShippingInfoModify() {
+		
+		return "/user/goods/goodsShippingInfoModify";
+	}
+	@GetMapping("/goodsCartList")
+	public String goodsCartList() {
+		
+		return "/user/goods/goodsCartList";
+	}
 }
+
