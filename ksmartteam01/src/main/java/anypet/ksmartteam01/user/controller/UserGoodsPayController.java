5530e9ed7cf7eb6b79cffbd66d56d9c8cb2adfb6
suessman <suessman11@gmail.com>
2022-09-30 15:53:31 +0900

    작업중

--- ksmartteam01/src/main/java/anypet/ksmartteam01/user/controller/UserGoodsPayController.java
+++ ksmartteam01/src/main/java/anypet/ksmartteam01/user/controller/UserGoodsPayController.java
@@ -1,5 +1,14 @@
 package anypet.ksmartteam01.user.controller;
 
-public class UserGoodsPayController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/user/goods")
+public class UserGoodsPayController {
+	@GetMapping("/goodsPay")
+	public String goodsPay() {
+		return "/user/goods/goodsPay";
+	}
 }
