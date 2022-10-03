d13db83381bff2961bec0f432ac721af23ebc9cf
오성준 <stationoflifeproject@gmail.com>
2022-10-04 02:02:59 +0900

    상품주문등

--- ksmartteam01/src/main/java/anypet/ksmartteam01/admin/controller/AdminGoodsPayController.java
+++ ksmartteam01/src/main/java/anypet/ksmartteam01/admin/controller/AdminGoodsPayController.java
@@ -1,5 +1,30 @@
 package anypet.ksmartteam01.admin.controller;
 
-public class AdminGoodsPayController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+public class AdminGoodsPayController {
+	@Controller
+	@RequestMapping("/admin/goods")
+	public class AdminGoodsListController {
+		
+		@GetMapping("/goodsOrderList")
+		public String goodsOrderList(Model model) {
+			
+			return "/admin/goods/goodsOrderList";
+		}
+		
+		@GetMapping("/goodsLargeCategoryInsert")
+		public String goodsLargeCategoryInsert(Model model) {
+			
+			return "/admin/goods/goodsLargeCategoryInsert";
+		}
+		@GetMapping("/goodsMediumCategoryInsert")
+		public String goodsMediumCategoryInsert(Model model) {
+			
+			return "/admin/goods/goodsMediumCategoryInsert";
+		}
+	}
 }
