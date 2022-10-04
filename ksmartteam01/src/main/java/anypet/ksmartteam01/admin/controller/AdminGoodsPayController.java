223963aae548d7ee7e201ba9dac0f686b40adb25
StationOfLifeProject <StationOfLifeProject@gmail.com>
2022-10-04 09:43:08 +0900

    10 04 커밋

--- ksmartteam01/src/main/java/anypet/ksmartteam01/admin/controller/AdminGoodsPayController.java
+++ ksmartteam01/src/main/java/anypet/ksmartteam01/admin/controller/AdminGoodsPayController.java
@@ -11,20 +11,26 @@ public class AdminGoodsPayController {
 	public class AdminGoodsListController {
 		
 		@GetMapping("/goodsOrderList")
-		public String goodsOrderList(Model model) {
+		public String goodsOrderList() {
 			
 			return "/admin/goods/goodsOrderList";
 		}
 		
 		@GetMapping("/goodsLargeCategoryInsert")
-		public String goodsLargeCategoryInsert(Model model) {
+		public String goodsLargeCategoryInsert() {
 			
 			return "/admin/goods/goodsLargeCategoryInsert";
 		}
 		@GetMapping("/goodsMediumCategoryInsert")
-		public String goodsMediumCategoryInsert(Model model) {
+		public String goodsMediumCategoryInsert() {
 			
 			return "/admin/goods/goodsMediumCategoryInsert";
+		}
+		
+		@GetMapping("/goodsCategoryList")
+		public String goodsCategoryList() {
+			
+			return "/admin/goods/goodsCategoryList";
 		}
 	}
 }
