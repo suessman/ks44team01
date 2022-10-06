c36c65d1014c3d8b1c5589cf325e28d02d8c05a8
suessman <suessman11@gmail.com>
2022-10-06 13:52:40 +0900

    에디터 데이터테이블 충돌 에러 해결

---
+++ ksmartteam01/src/main/resources/static/build/js/editor.js
@@ -0,0 +1,6 @@
+<!-- Editor -->
+<!-- bootstrap-wysiwyg -->
+<script th:src="@{/vendors/bootstrap-wysiwyg/js/bootstrap-wysiwyg.min.js}"></script>
+<script th:src="@{/vendors/jquery.hotkeys/jquery.hotkeys.js}"></script>	
+<!-- Switchery -->
+<script th:src="@{/vendors/switchery/dist/switchery.min.js}"></script>	
