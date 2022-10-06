c4c462433ee0ad8cf3bce3138e4d6df7b4f979d5
StationOfLifeProject <StationOfLifeProject@gmail.com>
2022-10-06 12:40:05 +0900

    service패키지 pull

---
+++ ksmartteam01/src/main/java/anypet/ks44team01/dto/goodsLargeCategory.java
@@ -0,0 +1,34 @@
+package anypet.ks44team01.dto;
+
+public class goodsLargeCategory {
+
+	private String categoryCode;
+	private String categoryName;
+	private String registrationDatetime;
+	
+
+	public String getCategoryCode() {
+		return categoryCode;
+	}
+	public void setCategoryCode(String categoryCode) {
+		this.categoryCode = categoryCode;
+	}
+	public String getCategoryName() {
+		return categoryName;
+	}
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+	public String getRegistrationDatetime() {
+		return registrationDatetime;
+	}
+	public void setRegistrationDatetime(String registrationDatetime) {
+		this.registrationDatetime = registrationDatetime;
+	}
+	
+	@Override
+	public String toString() {
+		return "goodsLargeCategory [categoryCode=" + categoryCode + ", categoryName=" + categoryName
+				+ ", registrationDatetime=" + registrationDatetime + "]";
+	}
+}
