88172afbc009650732d4fca475caf83a4e273c23
kimhyeonjin <hjkim@didimstory.com>
2022-10-11 09:13:27 +0900

    숙소등록 페이지 작업

--- ksmartteam01/src/main/java/anypet/ks44team01/service/AccommodationListService.java
+++ ksmartteam01/src/main/java/anypet/ks44team01/service/AccommodationListService.java
@@ -5,8 +5,11 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import anypet.ks44team01.dto.AccommodationCategory;
 import anypet.ks44team01.dto.AccommodationList;
+import anypet.ks44team01.dto.Region;
 import anypet.ks44team01.mapper.AccommodationListMapper;
+
 
 @Service
 @Transactional
@@ -16,8 +19,25 @@ public class AccommodationListService {
 		this.accommodationListMapper = accommodationListMapper;
 	}
 	
+	//숙소목록
 	public List<AccommodationList> getAccommodationList(){
 		List<AccommodationList> accommodationList = accommodationListMapper.getAccommodationList();
 		return accommodationList;
+	}
+	
+	public List<AccommodationCategory> getCategoryList(){
+		List<AccommodationCategory> accommodationCategory = accommodationListMapper.getCategoryList();
+		return accommodationCategory;
+	}
+	
+	public List<Region> getRegionList(){
+		List<Region> region = accommodationListMapper.getRegionList();
+		return region;
+	}
+	
+	//숙소등록
+	public void addAccommodation(AccommodationList accommodationList) {
+		int result = accommodationListMapper.addAccommodation(accommodationList);
+		System.out.println("회원가입 결과:" + result);
 	}
 }
