8d389b4c626e029ed48cb10f6bf3ab1c87cffb67
suessman <suessman11@gmail.com>
2022-10-18 17:46:31 +0900

    intro화면 작업진행, 메인페이지에 배경 화면 추가

--- ksmartteam01/src/main/java/anypet/ks44team01/controller/MainController.java
+++ ksmartteam01/src/main/java/anypet/ks44team01/controller/MainController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
-	@GetMapping("/")
+	@GetMapping("/user/mainPage")
 	public String main(Model model) {
 		return"main";
 	}
@@ -19,5 +19,11 @@ public class MainController {
 	@GetMapping("/admin/login")
 	public String login() {
 		return "/admin/login";
+	}
+
+	//시작화면
+	@GetMapping("/")
+	public String intro() {
+		return "/user/intro";
 	}
 }
